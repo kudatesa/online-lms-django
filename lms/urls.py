4f31101880330ddb1c89390afc57c7c6734b59ea
Kuda Tesa <kudatesa81@gmail.com>
2026-02-26 12:00:00 +0200

    Implement core project logic and final configurations

--- lms/urls.py
+++ lms/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
     path('assignments/', include('assignments.urls')),
